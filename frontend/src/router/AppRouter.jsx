ed940bc8ff485837683fafebea3a39f88d7ad476
kkang123 <one98kr@gmail.com>
2025-03-05 15:27:44 +0900

    ♻️ Add /chat route, update / route to navigate to Nickname

--- frontend/src/router/AppRouter.jsx
+++ frontend/src/router/AppRouter.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Chat from "../chat/chat";
+import Nickname from "../user/nickname";
 
 const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Nickname />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
     </BrowserRouter>
